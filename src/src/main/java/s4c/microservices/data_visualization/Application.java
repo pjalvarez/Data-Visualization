f9e698ab1cde3acbfa4c0428cd21694b53d0e7e8
Rafael Carballo <rcarballo@emergya.com>
2018-01-18 21:58:41 +0100

    V2

--- src/src/main/java/s4c/microservices/data_visualization/Application.java
+++ src/src/main/java/s4c/microservices/data_visualization/Application.java
@@ -3,6 +3,8 @@ package s4c.microservices.data_visualization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,9 +18,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableJpaRepositories(basePackages = "s4c.microservices.data_visualization.model")
 @ComponentScan("s4c.microservices.data_visualization")
 @EntityScan("s4c.microservices.data_visualization.model.entity")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-    }   
+    }
+
 }
+
+
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }   
+//}
